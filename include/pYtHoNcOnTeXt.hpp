a3e632f6990d3e9ac9e15327f28a9cd334502204
r15hil <rishilapple@gmail.com>
2020-03-04 12:23:30 +0000

    starting context

--- include/pYtHoNcOnTeXt.hpp
+++ include/pYtHoNcOnTeXt.hpp
@@ -1,8 +1,29 @@
 #ifndef pycon_hpp
 #define pycon_hpp
 
+#include <vector>
+#include <string>
+#include <iostream>
+
 class pycon {
 public:
+
+  pycon(){}
+
+  void addGlobal(std::string glob){
+    globals.push_back(glob);
+  }
+
+  void printGlobals(std::ostream& os){
+    for(int i = 0; i < globals.size(); i++){
+      os << std::endl;
+      os << globals[i];
+    }
+  }
+
+  void indent(std::ostream& os){
+
+  }
   
 private:
   std::vector<std::string> globals;
