339d48c58e9274b0f7694099ff75d3de74da6d45
woonmoon <gk518@ic.ac.uk>
2020-03-31 00:45:07 +0100

    ok ladies now let's get into formation

--- include/ast/misc/switch.hpp
+++ include/ast/misc/switch.hpp
@@ -11,6 +11,7 @@ public:
     void print(std::ostream& dst, pycon& con, int level) const {}
     void pythonGen(std::ostream& os) const {}
     void mipsGen(std::ostream& os, mipsCon& con, int dest=0) const {
+        con.switchy().labels={};
         statement->look(con);
     }
     void look(mipsCon& con) const {}
